c71879c26ace77d99f931916a95aab1e50f4bfe5
Heber Billings <heber.billings@gmail.com>
2021-12-03 22:29:26 -0700

    day04

---
+++ src/day04/day04b.js
@@ -0,0 +1,98 @@
+import * as util from '../util.js';
+import _ from 'lodash';
+
+// const input = 'input.test.txt';
+const input = 'input.txt';
+
+function checkBoard(board) {
+    for (let y = 0; y < 5; y++) {
+        let count = 0;
+        for (let x = 0; x < 5; x++) {
+            if (board[x][y] === '*') {
+                count++;
+            }
+        }
+        if (count === 5) {
+            return true;
+        }
+    }
+
+    for (let x = 0; x < 5; x++) {
+        let count = 0;
+        for (let y = 0; y < 5; y++) {
+            if (board[x][y] === '*') {
+                count++;
+            }
+        }
+        if (count === 5) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function main() {
+    const inputLines = util.loadInput(input);
+
+    const called = inputLines.shift();
+    const boards001 = _.chunk(inputLines, 5);
+
+    const randomNumbers = _.split(called, ',');
+    let boards = _.map(boards001, (board) => {
+        return _.map(board, (line) => {
+            return _.compact(_.split(line, ' '));
+        });
+    });
+
+    let winner = null;
+    let lastNum = null;
+    turn:
+        for (let i = 0; i < randomNumbers.length; i++) {
+            const call = randomNumbers[i];
+
+            console.log('call', call);
+
+            boards = _.map(boards, (board) => {
+                return _.map(board, (line) => {
+                    return _.map(line, (num) => {
+                        if (num === call) {
+                            return '*';
+                        } else {
+                            return num;
+                        }
+                    });
+                });
+            });
+
+            if (_.size(boards) === 1) {
+                if (checkBoard(boards[0])) {
+                    winner = boards[0];
+                    lastNum = call;
+                    break turn;
+                }
+            } else {
+                boards = _.filter(boards, (board) => {
+                    return !checkBoard(board);
+                });
+            }
+        }
+
+    console.log('winner', winner, lastNum);
+
+    const score = _.reduce(winner, (acc, line) => {
+        return acc + _.reduce(line, (acc2, num) => {
+            if (num !== '*') {
+                acc2 += _.parseInt(num);
+            }
+            return acc2
+        }, 0);
+    }, 0);
+
+    console.log('score', score);
+    console.log('lastNum', lastNum);
+    console.log('FINAL SCORE', score * _.parseInt(lastNum));
+    // 486
+}
+
+main();
