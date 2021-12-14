90cfa1ae63783dbbf02d07d6a34a9225436f0179
Heber Billings <heber.billings@gmail.com>
2021-12-13 22:35:49 -0700

    day14 part1

---
+++ src/day14/day14b.js
@@ -0,0 +1,46 @@
+import * as util from '../util.js';
+import _ from 'lodash';
+
+const input = 'input.test.txt';
+// const input = 'input.txt';
+
+function step(currentCode, map) {
+    const nextCode = [];
+
+    let prevChar = '';
+    _.forEach(currentCode, (char01) => {
+        if (prevChar) {
+            const middle = map[`${prevChar}${char01}`];
+            if (middle) {
+                nextCode.push(middle);
+            }
+        }
+
+        nextCode.push(char01)
+        prevChar = char01;
+    });
+
+    return nextCode;
+}
+
+function main() {
+    const [ strStart, ...inputLines] = util.loadInput(input);
+
+    const map = _.reduce(inputLines, (acc, line) => {
+        const [ from, add ] = line.split(' -> ');
+        acc[from] = add;
+        return acc;
+    }, {});
+
+    let code = strStart.split('');
+
+    for (let i = 1; i <= 40; i++) {
+        code = step(code, map);
+    }
+
+    const counts = _.countBy(code);
+
+    console.log('score', _.max(_.values(counts)) - _.min(_.values(counts)));
+}
+
+main();
