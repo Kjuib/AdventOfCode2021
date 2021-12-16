163e24812d4c3d605cda7c4961446a8b5ffe7c6a
hbillings <hbillings@cmsonline.com>
2021-12-16 07:52:44 -0700

    day 16 part2

--- src/day16/day16a.js
+++ src/day16/day16a.js
@@ -5,11 +5,11 @@ const input = 'input.txt';
 
 const versions = [];
 
-function decode(str) {
-    const version = str.substring(0, 3);
+function decode(binaryStr) {
+    const version = binaryStr.substring(0, 3);
     versions.push(version);
-    const type = str.substring(3, 6);
-    const rest = str.substring(6);
+    const type = binaryStr.substring(3, 6);
+    const rest = binaryStr.substring(6);
 
     if (type === '100') {
         let key = '1';
@@ -22,19 +22,50 @@ function decode(str) {
             current = current.substring(5);
         } while (key === '1')
 
-        console.log('value', value);
         const binaryValue = _.parseInt(value, 2);
-        console.log('binaryValue', binaryValue);
+
+        let literalValue = {
+            value: value,
+            binaryValue: binaryValue,
+            leftOvers: current
+        };
+        return literalValue;
     } else {
-        const length = rest.substring(0, 1) === '1' ? 11 : 15;
+        const lengthTypeId = rest.substring(0, 1);
+        let leftOvers = '';
+        if (lengthTypeId === '0') {
+            const subLength = _.parseInt(rest.substring(1, 16), 2);
+            let rest002 = rest.substring(16, 16 + subLength);
+            leftOvers = rest.substring(16 + subLength);
+            do {
+                rest002 = decode(rest002).leftOvers;
+            } while (!_.isEmpty(rest002));
+        } else {
+            const subPackets = _.parseInt(rest.substring(1, 12), 2);
+
+            let rest002 = rest.substring(12);
+            for (let i = 0; i < subPackets; i++) {
+                rest002 = decode(rest002).leftOvers;
+            }
+            leftOvers = rest002;
+        }
+
+        return {
+            leftOvers: leftOvers
+        };
     }
 
 }
 
 function main() {
-    // const inputLine = util.loadInput(input)[0];
+    const inputLine = util.loadInput(input)[0];
     // const inputLine = 'D2FE28';
-    const inputLine = '38006F45291200';
+    // const inputLine = '38006F45291200';
+    // const inputLine = 'EE00D40C823060';
+    // const inputLine = '8A004A801A8002F478';
+    // const inputLine = '620080001611562C8802118E34';
+    // const inputLine = 'C0015000016115A2E0802F182340';
+    // const inputLine = 'A0016C880162017C3686B18A3D4780';
 
     const hexDigits = inputLine.split('');
     const binaryStr = _.map(hexDigits, (d) => {
@@ -42,6 +73,12 @@ function main() {
     }).join('');
 
     decode(binaryStr);
+
+    const binaryVersions = _.map(versions, (str) => {
+        return _.parseInt(str, 2);
+    });
+
+    console.log('TOTAL VERSIONS', _.sum(binaryVersions));
 }
 
 main();
--- src/day16/day16b.js
+++ src/day16/day16b.js
@@ -1,52 +1,100 @@
 import * as util from '../util.js';
 import _ from 'lodash';
 
-// const input = 'input.test.txt';
 const input = 'input.txt';
 
-function step(currentCode, map) {
-    const nextCode = {};
+function decode(binaryStr) {
+    const version = binaryStr.substring(0, 3);
+    const type = binaryStr.substring(3, 6);
+    const rest = binaryStr.substring(6);
 
-    _.forEach(currentCode, (value, key) => {
-        const newPair = map[key];
-        nextCode[newPair[0]] = (nextCode[newPair[0]] || 0) + value;
-        nextCode[newPair[1]] = (nextCode[newPair[1]] || 0) + value;
-    });
+    if (type === '100') {
+        let key = '1';
+        let current = rest;
+        let value = '';
 
-    return nextCode;
+        do {
+            key = current.substring(0, 1);
+            value += current.substring(1, 5);
+            current = current.substring(5);
+        } while (key === '1')
+
+        const binaryValue = _.parseInt(value, 2);
+
+        return {
+            value: value,
+            binaryValue: binaryValue,
+            leftOvers: current
+        };
+    } else {
+        const lengthTypeId = rest.substring(0, 1);
+        let leftOvers;
+        let results = [];
+        if (lengthTypeId === '0') {
+            const subLength = _.parseInt(rest.substring(1, 16), 2);
+            let rest002 = rest.substring(16, 16 + subLength);
+            leftOvers = rest.substring(16 + subLength);
+            do {
+                let result = decode(rest002);
+                results.push(result.binaryValue);
+                rest002 = result.leftOvers;
+            } while (!_.isEmpty(rest002));
+        } else {
+            const subPackets = _.parseInt(rest.substring(1, 12), 2);
+
+            let rest002 = rest.substring(12);
+            for (let i = 0; i < subPackets; i++) {
+                let result = decode(rest002);
+                results.push(result.binaryValue);
+                rest002 = result.leftOvers;
+            }
+            leftOvers = rest002;
+        }
+
+        let result = {
+            leftOvers: leftOvers
+        }
+        if (type === '000') {
+            result.binaryValue = _.sum(results);
+        } else if (type === '001') {
+            result.binaryValue = _.reduce(results, (acc, current) => {
+                return acc * current;
+            }, 1);
+        } else if (type === '010') {
+            result.binaryValue = _.min(results);
+        } else if (type === '011') {
+            result.binaryValue = _.max(results);
+        } else if (type === '101') {
+            result.binaryValue = results[0] > results[1] ? 1 : 0;
+        } else if (type === '110') {
+            result.binaryValue = results[0] < results[1] ? 1 : 0;
+        } else if (type === '111') {
+            result.binaryValue = results[0] === results[1] ? 1 : 0;
+        }
+
+        return result;
+    }
+
 }
 
 function main() {
-    console.time();
-    const [ strStart, ...inputLines] = util.loadInput(input);
+    const inputLine = util.loadInput(input)[0];
+    // const inputLine = 'C200B40A82';
+    // const inputLine = '04005AC33890';
+    // const inputLine = '880086C3E88112';
+    // const inputLine = 'CE00C43D881120';
+    // const inputLine = 'D8005AC2A8F0';
+    // const inputLine = 'F600BC2D8F';
+    // const inputLine = '9C005AC2F8F0';
+    // const inputLine = '9C0141080250320F1802104A08';
 
-    const map = _.reduce(inputLines, (acc, line) => {
-        const [ from, add ] = line.split(' -> ');
-        acc[from] = [ `${from[0]}${add}`, `${add}${from[1]}` ];
-        return acc;
-    }, {});
+    const hexDigits = inputLine.split('');
+    const binaryStr = _.map(hexDigits, (d) => {
+        return _.padStart(_.parseInt(d, 16).toString(2), 4, '0');
+    }).join('');
 
-    let code = {};
-    for (let i = 0; i < strStart.length - 1; i++) {
-        const key = `${strStart[i]}${strStart[i + 1]}`;
-        code[key] = (code[key] || 0) + 1;
-    }
-
-    for (let i = 1; i <= 40; i++) {
-        code = step(code, map);
-    }
-
-    const counts = {};
-    _.forEach(code, (value, key) => {
-        counts[key[0]] = (counts[key[0]] || 0) + (value / 2);
-        counts[key[1]] = (counts[key[1]] || 0) + (value / 2);
-    });
-
-    const max = _.ceil(_.max(_.values(counts)));
-    const min = _.ceil(_.min(_.values(counts)));
-
-    console.log('score', max - min);
-    console.timeEnd();
+    const result = decode(binaryStr);
+    console.log('result', result);
 }
 
 main();
